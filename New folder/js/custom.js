cff032b82c0f4497603ff7ad5dd1ddfbd089d7cc
meetgorasia <meetgorasia.22@gmail.com>
2018-11-19 18:55:02 +1000

    gorasia

--- New folder/js/custom.js
+++ New folder/js/custom.js
@@ -32,13 +32,3 @@ $(document).ready(function () {
 
 
 });
-
-$(document).ready(function(){
-	$('.hamIcon').click(function(){
-		$('.hamIcon').toggleClass('hamActive');
-	});
-});
-
-function toggleSidebar(){
-	document.getElementsByClassName("left-sidebar")[0].classList.toggle('sideBarActive');
-}
