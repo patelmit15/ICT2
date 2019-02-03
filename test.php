c6e92144d35c60b606638e80b2425e3fdeb8817b
patelmit15 <42079625+patelmit15@users.noreply.github.com>
2019-02-03 23:02:21 +1000

    final

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+// echo "hii";
+
+// try{
+//     symlink("/home/artiders/laravel/storage/app/upload/","/home/artiders/public_html/storage/app/upload/");
+    
+// }
+// catch(Exception $e){
+//     echo "hiiassa";
+// }
